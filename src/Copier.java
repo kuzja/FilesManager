1bcebd988cab0e10a2bbc15f462711add8362e7f
Eugene <kuzmenko.evg@gmail.com>
2017-01-17 10:59:46 +0200

    Initial commit

---
+++ src/Copier.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.concurrent.TimeUnit;
+
+public class Copier implements Runnable {
+
+    public static long writtenBytes;
+    private final RandomAccessFile in;
+    private final RandomAccessFile out;
+    private int bufferSize;
+
+    public Copier(RandomAccessFile in, RandomAccessFile out, int bufferSize) {
+        this.in = in;
+        this.out = out;
+        this.bufferSize = bufferSize;
+    }
+
+    private void copy() throws IOException, InterruptedException {
+        byte[] buffer = new byte[bufferSize];
+        int byteCount = -1;
+
+        while (true) {
+
+            synchronized (in) {
+                byteCount = in.read(buffer);
+                TimeUnit.MILLISECONDS.sleep(5);
+            }
+
+            synchronized (out) {
+                if (byteCount == -1) {
+                    break;
+                }
+                writtenBytes = writtenBytes + byteCount;
+                out.write(buffer, 0, byteCount);
+                TimeUnit.MILLISECONDS.sleep(5);
+            }
+
+            TimeUnit.MILLISECONDS.sleep(1000);
+        }
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            copy();
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/FileManager.java
@@ -0,0 +1,232 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class FileManager {
+
+    private Scanner in;
+    private String pathFrom;
+    private String pathTo;
+    private int threadCount;
+    private int bufferSize;
+    private long copyFilesLength;
+
+    public FileManager() {
+        this.in = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) {
+
+        FileManager fm = new FileManager();
+
+        System.out.println("Specify copy from:");
+        fm.setPathFrom(fm.getUserDataString());
+        fm.checkPath(fm.getPathFrom());
+
+        System.out.println("Specify copy to:");
+        fm.setPathTo(fm.getUserDataString());
+
+        System.out.println("Specify threads count (minimum 2):");
+        fm.setThreadCount(Integer.parseInt(fm.getUserDataString()));
+
+        if (fm.getThreadCount() < 2) {
+            fm.setThreadCount(2);
+            System.out.println("Threads count is set 2");
+        }
+
+        System.out.println("Specify buffer size (Kb):");
+        fm.setBufferSize(Integer.parseInt(fm.getUserDataString()));
+
+        ArrayList<String> dirs = new ArrayList<>();
+        ArrayList<String> files = new ArrayList<>();
+
+        boolean isDirectory = fm.checkPath(fm.getPathFrom());
+
+        fm.copyFilesLength = fm.scanDirs(fm.getPathFrom(), dirs, files);
+
+        Operation op = fm.getOperationType();
+
+        if (isDirectory) {
+
+            for (String dir : dirs) {
+                File directory = new File(dir.replace(fm.getPathFrom(), fm.getPathTo()));
+                if (!directory.exists()) {
+                    directory.mkdirs();
+                }
+            }
+        }
+
+        Thread countDown = new Thread(fm.new CountDown());
+        countDown.start();
+
+        boolean success = true;
+
+        for (String file : files) {
+
+            String dest = file.replace(fm.getPathFrom(), fm.getPathTo());
+            String source = file;
+
+            if (!isDirectory) {
+                dest = fm.getPathTo() + dest;
+                source = fm.getPathFrom();
+            }
+
+            try (RandomAccessFile in = new RandomAccessFile(source, "r");
+                 RandomAccessFile out = new RandomAccessFile(dest, "rw");) {
+
+                ExecutorService executor = Executors.newFixedThreadPool(fm.getThreadCount());
+
+                for (int i = 0; i < fm.getThreadCount(); i++) {
+                    executor.execute(new Copier(in, out, fm.getBufferSize()));
+                }
+
+                executor.shutdown();
+
+                while (!executor.isTerminated()) {/*NOP*/}
+
+            } catch (IOException ex) {
+                success = false;
+                countDown.interrupt();
+                System.out.println("An error occurred while copying files. Operation terminated");
+                break;
+            }
+        }
+
+        if (success) {
+            countDown.interrupt();
+            System.out.println("File(s) has been successfully copied.");
+        }
+
+    }
+
+    private Operation getOperationType() {
+        if (new File(pathFrom).isDirectory() & new File(getPathTo()).isDirectory()) {
+            return Operation.DirToDest;
+        } else if ((!(new File(pathFrom).isDirectory()) & !(new File(getPathTo()).isDirectory()))) {
+            return Operation.FileToFile;
+        } else {
+            return Operation.FileToDir;
+        }
+    }
+
+    public int getThreadCount() {
+        return threadCount;
+    }
+
+    public void setThreadCount(int threadCount) {
+        this.threadCount = threadCount;
+    }
+
+    public int getBufferSize() {
+        return bufferSize;
+    }
+
+    public void setBufferSize(int bufferSize) {
+        this.bufferSize = bufferSize * 1024;
+    }
+
+    public String getPathFrom() {
+        return pathFrom;
+    }
+
+    public void setPathFrom(String pathFrom) {
+        this.pathFrom = pathFrom;
+    }
+
+    public String getPathTo() {
+        return pathTo;
+    }
+
+    public void setPathTo(String pathTo) {
+        this.pathTo = pathTo;
+    }
+
+    private boolean checkPath(String path) {
+
+        while (true) {
+
+            File file = new File(path);
+
+            if (!file.exists()) {
+                System.out.println("Specified path does not exist");
+                System.out.println("If you want other path write (y) or write (n) to exit");
+
+                if (getUserDataString().equals("y")) {
+                    System.out.println("Specify copy from:");
+                    path = getUserDataString();
+
+                    continue;
+                } else {
+                    System.exit(0);
+                }
+            }
+            setPathFrom(path);
+            return file.isDirectory();
+        }
+    }
+
+    private String getUserDataString() {
+        return in.nextLine();
+    }
+
+    private long scanDirs(String path, ArrayList<String> dirs, ArrayList<String> files) {
+
+        File f = new File(path);
+
+        File[] listFiles = f.listFiles();
+
+        if (f.isDirectory()) {
+
+            for (File file : listFiles) {
+
+                if (!file.isDirectory()) {
+                    copyFilesLength = copyFilesLength + file.length();
+                    files.add(file.getAbsolutePath());
+                }
+
+                if (file.isDirectory()) {
+                    dirs.add(file.getAbsolutePath());
+                    copyFilesLength = copyFilesLength + file.length();
+                    scanDirs(file.getPath(), dirs, files);
+                }
+            }
+        } else {
+
+            if (f.getName().charAt(f.getName().length() - 1) == '\\') {
+                files.add(f.getName());
+            } else {
+                files.add("\\" + f.getName());
+            }
+            copyFilesLength = f.length();
+        }
+        return copyFilesLength;
+    }
+
+    class CountDown implements Runnable {
+        @Override
+        public void run() {
+            try {
+                long copied = 0;
+                while (copyFilesLength != Copier.writtenBytes) {
+                    if (Thread.currentThread().isInterrupted()) {
+                        throw new InterruptedException();
+                    }
+                    TimeUnit.SECONDS.sleep(1);
+
+                    double changeSpeed = Copier.writtenBytes - copied;
+
+                    int remainingTime = (int) ((copyFilesLength - Copier.writtenBytes) / changeSpeed);
+
+                    copied = Copier.writtenBytes;
+
+                    System.out.printf("remaining time ......... %4ds %10.2f MB/s%n", remainingTime, changeSpeed / 1024 / 1024);
+                }
+            } catch (InterruptedException e) {/*NOP*/}
+        }
+    }
+}
